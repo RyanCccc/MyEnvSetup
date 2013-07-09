8d4dfaa9f70005a6c648ed3fabbf3352f2839361
RyanCccc <chenrd769@gmail.com>
2013-07-09 15:34:50 -0400

    Add notifications

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-    curl -Sso ~/.vim/autoload/pathogen.vim \
-        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
 if [ `find ~ -maxdepth 1 -name '.bashrc' | wc -l` -eq 0 ]; then
     echo '.bashrc Not exist'
+    echo 'Creating .bashrc file'
     touch ~/.bashrc
 else
     mv ~/.bashrc ~/.bashrc_backup
@@ -12,16 +9,29 @@ else
 fi
 cp .bashrc ~/
 cp .bash_aliases ~/
+echo 'Finish copying bash config'
 
 if [ `find ~ -maxdepth 1 -name '.vimrc' | wc -l` -eq 0 ]; then
     echo '.vimrc Not exist'
+    echo 'Creating .vimrc file'
     touch ~/.vimrc
 else
     mv ~/.vimrc ~/.vimrc_backup
     echo 'backup .vimrc to .vimrc_backup'
 fi
 cp .vimrc ~/
+echo 'Finish copying vim config'
 
+echo 'Installing vim-pathogen'
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+    curl -Sso ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+echo 'Installing vim bundles'
 cp -r bundle ~/.vim/
+
+echo 'Copying git config'
 cp .gitconfig ~/
+
+echo 'source .bashrc'
 source ~/.bashrc
