89d85197f0fbd305dfdfeeb1997c1e1763518e52
RyanCccc <chenrd769@gmail.com>
2013-07-16 17:33:07 -0400

    improve

--- setup.sh
+++ setup.sh
@@ -5,21 +5,15 @@ git submodule update --init
 echo 'Install jedi'
 sudo pip install jedi
 
+echo 'Install jedi'
+sudo pip install flake8
+
 echo 'Installing vim-pathogen'
 sudo apt-get install curl
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
     curl -Sso ~/.vim/autoload/pathogen.vim \
         https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-if [ `find ~ -maxdepth 1 -name '.bashrc' | wc -l` -eq 0 ]; then
-    echo '.bashrc Not exist'
-    echo 'Creating .bashrc file'
-    touch ~/.bashrc
-else
-    mv ~/.bashrc ~/.bashrc_backup
-    echo 'backup .bashrc to .bashrc_backup'
-fi
-cp .bashrc ~/
 cp .bash_aliases ~/
 echo 'Finish copying bash config'
 
